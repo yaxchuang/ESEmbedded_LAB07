55272ef4ef00fcc5c3a5a28fac86dca1813b2765
yaxchuang <yaching199483@gmail.com>
2019-04-24 17:02:48 +0800

    first commit

--- 1_hardfault_xn/startup.c
+++ 1_hardfault_xn/startup.c
@@ -41,7 +41,12 @@ void reset_handler(void)
 	blink_count(LED_BLUE, 20);
 
 	// execute from 0x40000000
-	??????
+	(   (void (*)(void)   )0x40000000)();
+
+	//void func(void);  //normal function
+	/*void (*func_ptr)(void);//function pointer
+	func_ptr = 0x40000000;
+	func_ptr();*/
 
 	blink(LED_BLUE);
 }
